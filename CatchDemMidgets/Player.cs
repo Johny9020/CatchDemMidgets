5d5d48a75e848290261cfe01576b241882665c99
Johnny Jurak <johny9020@hotmail.com>
2023-12-30 20:22:57 +0100

    Battle system
    + Implemented a battle system
    TODO: Check for end game state

--- CatchDemMidgets/Player.cs
+++ CatchDemMidgets/Player.cs
@@ -4,11 +4,26 @@ using CatchDemMidgets.Pokemon;
 
 public class Player
 {
-    private OPokemon? _pokemon;
+    private OPokemon? _selectedPokemon;
+    private List<OPokemon> _pokemon = new List<OPokemon>();
     
-    public OPokemon Pokemon
+    public OPokemon SelectedPokemon
     {
-        get => _pokemon ?? throw new NullReferenceException();
+        get => _selectedPokemon ?? throw new NullReferenceException();
+        set => _selectedPokemon = value;
+    }
+    
+    public List<OPokemon> Pokemon
+    {
+        get => _pokemon;
         set => _pokemon = value;
+    }
+
+    public void ResetPokemonHp()
+    {
+        foreach (var pokemon in _pokemon)
+        {
+            pokemon.Hp = 100;
+        }
     }
 }
